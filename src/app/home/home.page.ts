94665e046ca5d2bc2d4121fa349c46912a9a29dd
Matic Trebusak <matic.trebusak@razum.si>
2025-02-21 21:34:18 +0100

    Added tailwind

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -23,16 +23,7 @@ import {
       </ion-header>
 
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>
-          Start with Ionic
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://ionicframework.com/docs/components"
-            >UI Components</a
-          >
-        </p>
+        <div class="text-red-500">Test</div>
       </div>
     </ion-content>
   `,
