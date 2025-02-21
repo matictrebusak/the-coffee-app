8e4ffd42c99637b4aaa560c968b30d8bf3209013
Matic Trebusak <matic.trebusak@razum.si>
2025-02-21 21:15:16 +0100

    Moved template and styles to inline styles, made them default for ng schematics

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  template: `
+    <ion-header [translucent]="true">
+      <ion-toolbar>
+        <ion-title> Blank </ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content [fullscreen]="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <div id="container">
+        <strong>Ready to create an app?</strong>
+        <p>
+          Start with Ionic
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://ionicframework.com/docs/components"
+            >UI Components</a
+          >
+        </p>
+      </div>
+    </ion-content>
+  `,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class HomePage {
+export default class HomePage {
   constructor() {}
 }
