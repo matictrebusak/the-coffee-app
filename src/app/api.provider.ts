a1f6e082d8b5048b59e1e90e83328ee0dbe0e327
Matic Trebusak <matic.trebusak@razum.si>
2025-02-27 23:13:15 +0100

    Moved from config to environment variables

--- src/app/api.provider.ts
+++ src/app/api.provider.ts
@@ -1,26 +1,26 @@
 import { BASE_PATH } from './api/v1/variables';
 import { EnvironmentProviders, makeEnvironmentProviders } from '@angular/core';
-import { Configuration, ConfigurationParameters } from './api/v1';
+import { Configuration } from './api/v1';
 import { provideHttpClient } from '@angular/common/http';
-import { CONFIG } from './config/config';
+import { environment } from 'src/environments/environment';
 
-export function withBackendApiConfiguration(
-  configurationParameters: ConfigurationParameters = {}
-): Configuration {
-  return new Configuration({
-    // any default parameters
-    // basePath: 'my-default-api',
-    // overrides
-    ...configurationParameters,
-  });
-}
+// export function withBackendApiConfiguration(
+//   configurationParameters: ConfigurationParameters = {}
+// ): Configuration {
+//   return new Configuration({
+//     // any default parameters
+//     // basePath: 'my-default-api',
+//     // overrides
+//     ...configurationParameters,
+//   });
+// }
 
 export function provideApi(
   withConfiguration?: Configuration
 ): EnvironmentProviders {
   return makeEnvironmentProviders([
-    { provide: Configuration, useValue: withConfiguration },
-    { provide: BASE_PATH, useValue: CONFIG.TABLET_IP },
+    // { provide: Configuration, useValue: withConfiguration },
+    { provide: BASE_PATH, useValue: environment.tabletIP },
     provideHttpClient(),
   ]);
 }
--- src/app/config/config.ts
+++
@@ -1,3 +0,0 @@
-export let CONFIG = {
-  TABLET_IP: 'http://localhost:8080/api/v1',
-};
